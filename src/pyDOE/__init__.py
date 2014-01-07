214f75012b21177df2c87fc9c0f1ae9d5d0e8eb7
Dylan Harp <dharp73@gmail.com>
2014-01-06 22:21:12 -0700

    Added pyDOE package

---
+++ src/pyDOE/__init__.py
@@ -0,0 +1,18 @@
+"""
+================================================================================
+pyDOE: Design of Experiments for Python
+================================================================================
+
+Author: Abraham Lee
+Copyright: 2013
+
+"""
+__author__ = 'Abraham Lee'
+__version__ = '0.3'
+
+from doe_box_behnken import *
+from doe_composite import *
+from doe_factorial import *
+from doe_lhs import *
+from doe_fold import *
+from doe_plackett_burman import *
