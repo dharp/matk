0daf696c6cedce77e54e4c89c2e920a2da5ac783
Dylan Harp <dharp@lanl.gov>
2015-04-12 20:26:46 -0600

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+# PyTOUGH setup script
+from distutils.core import setup
+import os
+
+os.chdir('src')
+
+setup(name='MATK',
+	version='0.0.0',
+	description='Model Analysis ToolKit (MATK) - Python toolkit for model analysis',
+	author='Dylan R. Harp',
+	author_email='dharp@lanl.gov',
+	url='matk.lanl.gov',
+	license='LGPL',
+	packages=[
+		'emcee',
+		'lmfit',
+		'lmfit.uncertainties',
+		'pyDOE'],
+	py_modules=['matk','lhs','parameter','sampleset','minimizer','pest_io','ordereddict','observation','__init__'],
+	)
