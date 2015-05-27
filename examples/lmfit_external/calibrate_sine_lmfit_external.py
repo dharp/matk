644dc6296600bfbb3eb87822669b3b2eff405e56
Dylan Robert Harp <dharp@lanl.gov>
2015-05-26 20:49:28 -0600

    Added lmfit_external example

---
+++ examples/lmfit_external/calibrate_sine_lmfit_external.py
@@ -0,0 +1,55 @@
+# Calibration example modified from lmfit webpage
+# (http://cars9.uchicago.edu/software/python/lmfit/parameters.html)
+# This example demonstrates how to calibrate with an external code 
+# The idea is to replace `python sine.py` in run_extern with any
+# terminal command to run your model.
+import sys,os
+import numpy as np
+from matplotlib import pyplot as plt
+from multiprocessing import freeze_support
+from subprocess import Popen,PIPE,call
+from matk import matk, pest_io
+import cPickle as pickle
+
+def run_extern(params):
+    pest_io.tpl_write(params,'../sine.tpl','sine.py')
+    ierr = call('python sine.py', shell=True)
+    out = pickle.load(open('sine.pkl','rb'))
+    return out
+
+# create data to be fitted
+x = np.linspace(0, 15, 301)
+np.random.seed(1000)
+data = (5. * np.sin(2 * x - 0.1) * np.exp(-x*x*0.025) +
+        np.random.normal(size=len(x), scale=0.2) )
+
+# Create MATK object
+p = matk(model=run_extern)
+
+# Create parameters
+p.add_par('amp', value=10, min=0.)
+p.add_par('decay', value=0.1)
+p.add_par('shift', value=0.0, min=-np.pi/2., max=np.pi/2.)
+p.add_par('omega', value=3.0)
+
+# Create observation names and set observation values
+for i in range(len(data)):
+    p.add_obs('obs'+str(i+1), value=data[i])
+
+# Look at initial fit
+init_vals = p.forward(workdir='initial',reuse_dirs=True)
+plt.plot(x,data, 'k+')
+plt.plot(x,p.simvalues, 'r')
+plt.title("Before Calibration")
+plt.show(block=True)
+
+# Calibrate parameters to data, results are printed to screen
+p.lmfit(cpus=2,workdir='calib')
+
+# Look at calibrated fit
+plt.plot(x,data, 'k+')
+plt.plot(x,p.simvalues, 'r')
+plt.title("After Calibration")
+plt.show()
+
+
---
+++ examples/lmfit_external/sine.tpl
@@ -0,0 +1,24 @@
+ptf %
+import numpy as np
+import cPickle as pickle
+
+# define objective function: returns the array to be minimized
+def sine_decay():
+    """ model decaying sine wave, subtract data"""
+    amp = %amp%
+    shift = %shift%
+    omega = %omega%
+    decay = %decay%
+
+    x = np.linspace(0, 15, 301)
+    model = amp * np.sin(x * omega + shift) * np.exp(-x*x*decay)
+
+    obsnames = ['obs'+str(i) for i in range(1,model.shape[0]+1)]
+    return dict(zip(obsnames,model))
+
+
+if __name__== "__main__":
+    out = sine_decay()
+    pickle.dump(out,open('sine.pkl', 'wb'))
+
+
