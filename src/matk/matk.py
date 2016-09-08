b1b7d32592389bbeb1a4c212a0ee6f8fef1c292b
Dylan Robert Harp <dharp73@gmail.com>
2016-09-07 18:50:11 -0600

    Modified log file writing in parallel method

--- src/matk/matk.py
+++ src/matk/matk.py
@@ -844,8 +844,11 @@ class matk(object):
 
         results = [[numpy.NAN]]*len(parsets)
         for i in range(len(parsets)):
+            if logfile and i == 0: 
+                f = open(logfile, 'w')
+                f.write("Number of parameters: %d\n" % len(self.pars) )
+                f.flush()
             lst_ind, smp_ind, resp = resultsq.get()
-            if logfile: f = open(logfile, 'w')
             if isinstance( resp, str):
                 if logfile: 
                     f.write(resp+'\n')
@@ -857,7 +860,6 @@ class matk(object):
                 if verbose or logfile:
                     if i == 0:
                         if logfile: 
-                            f.write("Number of parameters: %d\n" % len(self.pars) )
                             f.write("Number of responses: %d\n" % len(self.obs) )
                         s = "%-8s" % 'index'
                         for nm in self.parnames:
