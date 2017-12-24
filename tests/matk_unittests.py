75b0e5ea9ea5e466a96d5e17540f10aac309507a
Dylan Harp <dharp@lanl.gov>
2017-12-24 15:00:45 -0600

    Small changes to sobol test and example

--- tests/matk_unittests.py
+++ tests/matk_unittests.py
@@ -321,6 +321,8 @@ class Tests(unittest.TestCase):
         self.assertTrue( result2.fun < 1.e-8, 'Objective function for Ackley problem is larger than tolerance of 1.e-8: ' + str(result.fun) )
 
     def testsobol(self):
+        ''' This test is based on the test problem at: http://salib.readthedocs.io/en/latest/getting-started.html#testing-installation
+        '''
         sys.path.append(os.path.join('..','src','matk'))
         from SALib.test_functions import Ishigami
         def myIshigami(pars):
