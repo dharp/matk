e8eab14963898d959df31b70f1a4ea04a6d22f28
Veronika Vasylkivska <veronika.vasylkivska@netl.doe.gov>
2022-11-23 07:54:18 -0800

    Import update for compatibility with Python 3.10 (Python 3.9 should still work).

--- src/matk/ordereddict.py
+++ src/matk/ordereddict.py
@@ -23,7 +23,7 @@
 try:
     from UserDict import DictMixin
 except ImportError:
-    from collections import MutableMapping as DictMixin
+    from collections.abc import MutableMapping as DictMixin
 
 class OrderedDict(dict, DictMixin):
 
